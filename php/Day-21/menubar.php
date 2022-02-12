a80cef76f8e5e7c9c15246b0a5ed45e0cc39fca7
Tajul Islam <hksukhy@gmail.com>
2022-02-12 14:17:17 +0600

    until 12feb

---
+++ php/Day-21/menubar.php
@@ -0,0 +1,66 @@
+<link rel="stylesheet" href="menubar.css">
+<ul>
+    <li><a href="#" class="active">Home</a></li>
+    <li><a href="#">Accounts</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">About</a></li>
+</ul>
+
+<div class="content">
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+</div>
---
+++ php/Day-21/nevbar.php
@@ -0,0 +1,67 @@
+<div class="menu">
+    <ul>
+        <li><a href="#" class="active">Home</a></li>
+        <li><a href="#">Accounts</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#" class="active" style="float:right;">Home</a></li>
+    </ul>
+</div>
+<div class="content">
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+    <h1>amar sonar bangla</h1>
+    <h3>Nothing else</h3>
+</div>
